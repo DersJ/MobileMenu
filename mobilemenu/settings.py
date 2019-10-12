91cd6ad3f6c6e114688b0618c4f6f71cef53f55f
Anders Juengst <anders.juengst@teradata.com>
2019-10-12 13:31:42 -0400

    update allowed hosts

--- mobilemenu/settings.py
+++ mobilemenu/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['mobile-menu.herokuapp.com']
+ALLOWED_HOSTS = ['mobile-menu.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
